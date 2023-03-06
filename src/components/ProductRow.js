2bd0ef03b60d8d988c5e9ce93e4f0cd9f90059bd
Hernando Cardenas <h3rc4rm@gmail.com>
2023-03-06 09:24:08 +0100

    wip

---
+++ src/components/ProductRow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductRow = (props) => {
+  const { inStock, name, price } = props;
+  const color = inStock ? 'black' : 'red';
+  return (
+    <tr>
+      <td style={{ color: color }}>{name}</td>
+      <td>{price}</td>
+    </tr>
+  );
+};
+
+export default ProductRow;
---
+++ src/components/ProductTable.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ProductRow from './ProductRow';
+
+const ProductTable = (props) => {
+  const { products } = props;
+  return (
+    <table className="product-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => {
+          return (
+            <ProductRow
+              key={product.id}
+              inStock={product.inStock}
+              name={product.name}
+              price={product.price}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default ProductTable;
